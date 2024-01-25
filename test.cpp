182ce2cda133e42b5f205c34acb29299c5a65b78
taektoung <ltk99469946@gmail.com>
2024-01-25 20:45:13 +0900

    first

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+double roundToTwoDecimalPlaces(double number) {
+    return std::round(number * 100.0) / 100.0;
+}
+
+int main() {
+    double originalNumber = 6.33000;
+    
+    double roundedNumber = roundToTwoDecimalPlaces(originalNumber);
+
+    // 소수점 이하 두 자리까지 출력
+    std::cout << std::fixed << std::setprecision(2) << roundedNumber << std::endl;
+
+    return 0;
+}
